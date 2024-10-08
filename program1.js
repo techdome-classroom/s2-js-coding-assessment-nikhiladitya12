8e1cb82824832fedd47711c0319f6eba803bf305
Nikhil Aditya <nikhiladitya.22@gmail.com>
2024-10-08 14:03:05 +0530

    Both Problems Solved

--- program1.js
+++ program1.js
@@ -3,9 +3,27 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const stack = [];
+    const matchingBrackets = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
     
+    for (let char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char); 
+        } else if (char === ')' || char === '}' || char === ']') {
+            if (stack.length === 0 || stack.pop() !== matchingBrackets[char]) {
+                return false; 
+            }
+        }
+    }
+    
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
+
 
 
--- program2.js
+++ program2.js
@@ -3,8 +3,31 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
+    let total = 0;
+    let n = s.length;
+
+    for (let i = 0; i < n; i++) {
+        let currentVal = romanMap[s[i]];
+        let nextVal = i < n - 1 ? romanMap[s[i + 1]] : 0;
+        
+        if (currentVal < nextVal) {
+            total -= currentVal;  
+        } else {
+            total += currentVal;  
+        }
+    }
+    
+    return total;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
